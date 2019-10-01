7a6267230ac461ad221a2841b35e4a31eebe5ccb
Unknown <juanmg0511@gmail.com>
2019-10-01 01:25:10 -0300

    update ej4

--- TP1py/TP1Ejercicio4.py
+++ TP1py/TP1Ejercicio4.py
@@ -1,13 +1,35 @@
 from matplotlib import pyplot as plt
+# from matplotlib import pyplot as plt
 import numpy as np
 
+#Generamos los puntos
 tam_muestra = 1000
-
 u1 = np.random.uniform(0, 1, tam_muestra)
 u2 = np.random.uniform(0, 1, tam_muestra)
 
-u1 = u1 * 10 +5
-u2 = u2 * 10 +5
+#Definimos el angulo de rotacion
+alpha = np.radians(45)
 
+#La matriz de rotacin es
+R = np.array(( (np.cos(alpha), -np.sin(alpha)),
+               (np.sin(alpha),  np.cos(alpha)) ))
+
+#Calculamos los vectores finales, rotados 45º
+F = list()
+for i in range(999):
+    V = np.array((u1[i],u2[i]))
+    F.append(R.dot(V))
+    
+Fu1,Fu2 = zip(*F)
+Fu1 = np.array(Fu1)
+Fu2 = np.array(Fu2)
+
+#Desplazamos el rombo
+Fu1 = Fu1 * 10 + 10
+Fu2 = Fu2 * 10 + 3
+
+#Graficamos
 plt.scatter(u1, u2)
+plt.show()
+plt.scatter(Fu1,Fu2)
 plt.show()
