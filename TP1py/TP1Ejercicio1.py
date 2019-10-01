9b5544b9a18986d14d4badd640dd3de75df090e8
martinBosch <martinbosch17@gmail.com>
2019-10-01 20:30:10 -0300

    Saco el GCL y las constantes en archivos apartes para poder importarlos en los demas ejercicios y no tener que repetir el codigo

--- TP1py/TP1Ejercicio1.py
+++ TP1py/TP1Ejercicio1.py
@@ -9,45 +9,13 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+from gcl import GCL, GCL01
+from constantes import m, a, c, x0
+
+
 #Tamaño gráficos
 plt.style.use('default')
 plt.rcParams['figure.figsize'] = (15, 10)
-
-
-# GCL
-class GCL:
-    """Generador Lineal Congruente
-
-    atributos:
-     m: el modulo
-     a: el multiplicador
-     c: el incremento
-     xn: ultimo valor generado
-    """
-
-    def __init__(self, m, a, c, x0):
-        self.m = m
-        self.a = a
-        self.c = c
-        self.xn = x0
-
-    def generar_numero_aleatorio(self):
-        self.xn = (self.a * self.xn + self.c) % self.m
-        return self.xn
-
-    def generar_muestra(self, muestra_largo):
-        muestra = []
-        for i in range(muestra_largo):
-            muestra.append(self.generar_numero_aleatorio())
-
-        return muestra
-
-
-m = 2**32
-a = 1013904223
-c = 1664525
-padrones = [93272, 92028, 79979, 96749]
-x0 = int(sum(padrones)/len(padrones))
 
 
 gcl = GCL(m, a, c, x0)
@@ -56,14 +24,6 @@ primeros_diez = gcl.generar_muestra(tam_muestra)
 print("Primeros 10 números generados:")
 print(primeros_diez)
 print("\n")
-
-
-class GCL01(GCL):
-    """Generador Lineal Congruente con distribucion [0, 1]
-    """
-
-    def generar_numero_aleatorio(self):
-        return super().generar_numero_aleatorio()/m
 
 
 gcl01 = GCL01(m, a, c, x0)
--- TP1py/TP1Ejercicio2.py
+++ TP1py/TP1Ejercicio2.py
@@ -11,48 +11,13 @@ from matplotlib import pyplot as plt
 from math import exp
 from math import log
 
+from gcl import GCL, GCL01
+from constantes import m, a, c, x0
+
+
 #Tamaño gráficos
 plt.style.use('default')
 plt.rcParams['figure.figsize'] = (15, 10)
-
-m = 2**32
-a = 1013904223
-c = 1664525
-
-padrones = [93272, 92028, 79979, 96749]
-x0 = int(sum(padrones)/len(padrones))
-
-class GCL_01():
-
-    """Generador Lineal Congruente con distribucion [0, 1]
-    atributos:
-     m: el modulo
-     a: el multiplicador
-     c: el incremento
-     xn: ultimo valor generado
-    """
-
-    def __init__(self, m, a, c, x0):
-        self.m = m
-        self.a = a
-        self.c = c
-        self.xn = x0
-
-    def generar_numero_aleatorio(self):
-        self.xn = (self.a * self.xn + self.c) % self.m
-        return self.xn
-
-    def generar_numero_aleatorio_uniforme(self):
-        return self.generar_numero_aleatorio()/m
-
-    def generar_secuencia(secuencia_largo):
-        gcl = GCL_01(m, a, c, x0)
-        secuencia = []
-        for i in range(secuencia_largo):
-            secuencia.append(gcl.generar_numero_aleatorio_uniforme())
-
-        return secuencia
-
 
 
 def f(t):
@@ -62,16 +27,15 @@ def f(t):
     elif t >= 10:
         return 3/5 * lambda_ * exp(-1 * lambda_ * (t - 10))
 
+
 x = np.arange(0.0, 100.0, 1)
 y = np.array(list(map(f, x)))
-
-
 print("Graficando función de densidad de probabilidad del 2.a:")
+plt.title('f(t)')
 plt.plot(x, y)
 plt.show()
 
 
-print("Graficando función de probabilidad acumulada del 2.b:")
 def F(t):
     lambda_ = 1/15
     if t >= 0 and t < 10:
@@ -79,13 +43,15 @@ def F(t):
     elif t >= 10:
         return (10/25) + (3/5) - (3/5) * exp(-1 * lambda_ * (t - 10))
 
+
 x = np.arange(0.0, 100.0, 1)
 y = np.array(list(map(F, x)))
-
+print("Graficando función de probabilidad acumulada del 2.b:")
+plt.title('F(t)')
 plt.plot(x, y)
 plt.show()
 
-print("Graficando función inversa de la función de probabilidad acumulada del 2.b:")
+
 def F_inversa(t):
     lambda_ = 1/15
     if t >= 0 and t < 10/25:
@@ -93,16 +59,19 @@ def F_inversa(t):
     elif t >= 10/25:
         return (-1/lambda_) * log( (5/3) - (5/3) * t) + 10
 
+
 x = np.arange(0.0, 1.0, 0.05)
 y = np.array(list(map(F_inversa, x)))
-
+print("Graficando función inversa de la función de probabilidad acumulada del 2.b:")
+plt.title('F_inversa(t)')
 plt.plot(x, y)
 plt.show()
 
 
+gcl01 = GCL01(m, a, c, x0)
 tam_muestra = 100000
 # muestra de numeros aleatorios con distrib uniforme [0, 1]
-muestra = GCL_01.generar_secuencia(tam_muestra)
+muestra = gcl01.generar_muestra(tam_muestra)
 
 print("Aplicando método de la transformada")
 y = np.array(list(map(F_inversa, muestra)))
