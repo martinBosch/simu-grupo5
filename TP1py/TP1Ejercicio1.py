2f480771af78e6a829ad0ce9e1298ac14ee42232
martinBosch <martinbosch17@gmail.com>
2019-10-01 20:15:49 -0300

    Corrijo cosas de codigo de python para no repetir codigo

--- TP1py/TP1Ejercicio1.py
+++ TP1py/TP1Ejercicio1.py
@@ -13,8 +13,8 @@ import matplotlib.pyplot as plt
 plt.style.use('default')
 plt.rcParams['figure.figsize'] = (15, 10)
 
-#GCL
 
+# GCL
 class GCL:
     """Generador Lineal Congruente
 
@@ -35,49 +35,41 @@ class GCL:
         self.xn = (self.a * self.xn + self.c) % self.m
         return self.xn
 
+    def generar_muestra(self, muestra_largo):
+        muestra = []
+        for i in range(muestra_largo):
+            muestra.append(self.generar_numero_aleatorio())
+
+        return muestra
 
 
 m = 2**32
 a = 1013904223
 c = 1664525
-
 padrones = [93272, 92028, 79979, 96749]
 x0 = int(sum(padrones)/len(padrones))
 
 
-def generar_secuencia(secuencia_largo):
-    gcl = GCL(m, a, c, x0)
-    secuencia = []
-    for i in range(secuencia_largo):
-        secuencia.append(gcl.generar_numero_aleatorio())
-
-    return secuencia
-
+gcl = GCL(m, a, c, x0)
 tam_muestra = 10
-primeros_diez = generar_secuencia(tam_muestra)
+primeros_diez = gcl.generar_muestra(tam_muestra)
 print("Primeros 10 números generados:")
 print(primeros_diez)
 print("\n")
 
 
-class GCL_01(GCL):
+class GCL01(GCL):
     """Generador Lineal Congruente con distribucion [0, 1]
     """
 
     def generar_numero_aleatorio(self):
         return super().generar_numero_aleatorio()/m
 
-    def generar_secuencia(secuencia_largo):
-        gcl = GCL_01(m, a, c, x0)
-        secuencia = []
-        for i in range(secuencia_largo):
-            secuencia.append(gcl.generar_numero_aleatorio())
 
-        return secuencia
-
+gcl01 = GCL01(m, a, c, x0)
 tam_muestra = 100000
 print("Generando secuencia de "+str(tam_muestra)+" valores")
-muestra = generar_secuencia(tam_muestra)
+muestra = gcl01.generar_muestra(tam_muestra)
 
 print("Generando histograma...")
 plt.hist(muestra, 'sturges')
